a999a6ff725748efd6c0c341536d9e451deb4d29
Tate Johnson <tate@tatey.com>
2013-07-06 20:26:28 +1000

    Formatting.
    * Single quote strings.
    * Whitepsace.

--- lib/generators/mocha/rails/install_generator.rb
+++ lib/generators/mocha/rails/install_generator.rb
@@ -1,15 +1,15 @@
 module Mocha
   module Rails
     class InstallGenerator < ::Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
-      class_option :mount, :aliases => '-m', :type => :string, :default => 'mocha', :desc => 'mounts engine at specified MOUNT route'
+      class_option :mount, :aliases => '-m', :type => :string, :default => 'mocha', :desc => 'Mounts engine at <MOUNT>'
 
-      desc 'installs mocha-rails into your application'
-      def install    
+      desc 'Installs mocha-rails into your application'
+      def install
         route "mount Mocha::Rails::Engine => '#{options[:mount]}' unless Rails.env.production?"
 
-        test_dir = Dir.exists?(::Rails.root.join "spec") ? "spec" : "test"
+        test_dir = Dir.exists?(::Rails.root.join 'spec') ? 'spec' : 'test'
         copy_file 'mocha-suite.js', "#{test_dir}/javascripts/mocha-suite.js"
       end
     end
