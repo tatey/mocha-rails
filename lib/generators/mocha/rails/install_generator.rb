739aadbd4d0307c07f0ac931352b1a2b88ea0be9
Tate Johnson <tate@tatey.com>
2013-07-06 20:19:19 +1000

    Fix constant scope.

--- lib/generators/mocha/rails/install_generator.rb
+++ lib/generators/mocha/rails/install_generator.rb
@@ -1,6 +1,6 @@
 module Mocha
   module Rails
-    class InstallGenerator < Rails::Generators::Base
+    class InstallGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
 
       class_option :mount, :aliases => '-m', :type => :string, :default => 'mocha', :desc => 'mounts engine at specified MOUNT route'
@@ -9,7 +9,7 @@ module Mocha
       def install    
         route "mount Mocha::Rails::Engine => '#{options[:mount]}' unless Rails.env.production?"
 
-        test_dir = Dir.exists?(Rails.root.join "spec") ? "spec" : "test"
+        test_dir = Dir.exists?(::Rails.root.join "spec") ? "spec" : "test"
         copy_file 'mocha-suite.js', "#{test_dir}/javascripts/mocha-suite.js"
       end
     end
